9a7d3710e330bf3118f58f217343b55bcbef47d0
gilvania moreira <moreiragil84@gmail.com>
2024-03-12 02:43:06 -0300

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { ThemeProvider } from 'styled-components'
+import Globalstyles from './styles/global';
+import theme from './styles/theme'
+
+import { Details } from './pages/details'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <Globalstyles />
+      <Details />
+    </ThemeProvider>
+  </React.StrictMode>,
+)
