cd2ab70b7f961dd88149f620988ac94b324f1a62
gilvania moreira <moreiragil84@gmail.com>
2024-03-13 16:01:24 -0300

    feat: image user

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,18 @@
-import  { Container } from './styles';
+import  { Container, Profile } from './styles';
 
 export function Header() {
     return (
         <Container>
-            
+            <Profile>
+                <img 
+                    src="https://github.com/GilvaniaMoreira.png"
+                    alt="Foto do uauário"
+                />
+                <div>
+                    <span>Bem-vindo</span>
+                    <strong>Gilvânia Moreira</strong>
+                </div>
+            </Profile>
         </Container>
     );
 }
