232e6a674f61acdcd47249d401c01651c55a640e
gilvania moreira <moreiragil84@gmail.com>
2024-03-21 14:33:44 -0300

    feat: singIn

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { ThemeProvider } from 'styled-components'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from 'styled-components';
 import Globalstyles from './styles/global';
 
-import theme from './styles/theme'
+import theme from './styles/theme';
 
-import { Home } from './pages/Home'
-
+import { SingIn } from './pages/SingIn';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <Globalstyles />
-      <Home />
+      <SingIn />
     </ThemeProvider>
   </React.StrictMode>,
 )
