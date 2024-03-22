32c5bc0e5798632d7cf80f193912994e89f6836c
gilvania moreira <moreiragil84@gmail.com>
2024-03-22 12:15:37 -0300

    feat: new page

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,13 @@ import Globalstyles from './styles/global';
 
 import theme from './styles/theme';
 
-import { Profile } from './pages/Profile';
+import { New } from './pages/New';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <Globalstyles />
-      <Profile />
+      <New />
     </ThemeProvider>
   </React.StrictMode>,
 )
